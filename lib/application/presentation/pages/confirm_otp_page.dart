5432a16f3bd7db232b0e0b55046c462901c8d438
vandinh <trandinhcn@gmail.com>
2022-12-26 15:02:09 +0700

    unit test login

--- lib/application/presentation/pages/confirm_otp_page.dart
+++ lib/application/presentation/pages/confirm_otp_page.dart
@@ -1,4 +1,5 @@
 import 'package:base_bloc_flutter/application/bloc/blocs.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_core/flutter_core.dart' as core;
 
@@ -35,7 +36,7 @@ class OtpConfirmListener extends StatelessWidget {
     return core.BlocListener<OtpConfirmBloc, OtpConfirmState>(
       listenWhen: (previous, current) {
         return current.errMessage != null ||
-            previous.isLoading != current.isLoading||
+            previous.isLoading != current.isLoading ||
             previous.isSuccess != current.isSuccess;
       },
       listener: (context, state) {
